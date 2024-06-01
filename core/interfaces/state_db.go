eda3cbe0d441fedc7c4b509c97b8d0ed0c016386
Shoetan <emmysoetan@gmail.com>
2024-06-01 16:26:16 +0100

    fixed import cycle

--- core/interfaces/state_db.go
+++ core/interfaces/state_db.go
@@ -1,31 +1,9 @@
-// Copyright 2023 The concrete-geth Authors
-//
-// The concrete-geth library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The concrete library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the concrete library. If not, see <http://www.gnu.org/licenses/>.
-
-//go:build !tinygo
-
-// This file will be replaced by interface_tinygo.go when building with tinygo
-// to prevent compatibility issues.
-
-package api
+package interfaces
 
 import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/holiman/uint256"
-	"github.com/ethereum/go-ethereum/core/interfaces"
-	//cc_api "github.com/ethereum/go-ethereum/concrete/api"
 )
 
 type StateDB interface {
@@ -54,7 +32,5 @@ type StateDB interface {
 	// SetEphemeralState(addr common.Address, key common.Hash, value common.Hash)
 	// GetEphemeralState(addr common.Address, key common.Hash) common.Hash
 }
-
-var _ interfaces.StateDB = StateDB(nil)
 
 
